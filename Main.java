407a9379977e0aa265246ccab16cdf6c61de864e
Chris0016 <57505919+Chris0016@users.noreply.github.com>
2022-07-30 14:44:23 -0400

    Added Styling
    Changes:
    Google Fonts added.
    Applying coloring to reset button.
    Re-positioning of items in the graphics.

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("tictactoe.fxml"));
         primaryStage.setTitle("Tic Tac Toe");
-        Scene scene  = new Scene(root, 600, 600);
+        primaryStage.getIcons().add(new Image("file:tictactoe-icon.png"));
+        Scene scene  = new Scene(root, 600, 500);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
